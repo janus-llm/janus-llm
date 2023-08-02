156b8496b340956791cd320a197ad28db73f7676
Michael Doyle <mdoyle@mitre.org>
2023-08-02 11:27:42 -0700

    [WIP] Adding LangChain to openai.py

---
+++ janus/llm/custom.py
@@ -0,0 +1,9 @@
+from langchain.llms import LLM
+
+
+class CustomLLM:
+    """A class to interact with the LLMs."""
+
+    def __init__(self) -> None:
+        """Initialize the LLM."""
+        pass
--- janus/llm/openai.py
+++ janus/llm/openai.py
@@ -1,10 +1,16 @@
 """A module that defines..."""
-import time
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 
-import openai
+from langchain.llms import OpenAI
+from langchain.prompts.chat import (
+    AIMessagePromptTemplate,
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
 
 from ..utils.logger import create_logger
+from .custom import CustomLLM
 
 log = create_logger(__name__)
 
@@ -36,37 +42,44 @@ COST_PER_MODEL: Dict[str, float] = {
 }
 
 
-class OpenAI:
+class MyOpenAI(CustomLLM):
     """A class to interact with the OpenAI LLMs."""
 
     def __init__(
         self,
         model: str,
-        open_ai_api_key: str,
-        organization: Optional[str] = None,
+        openai_api_key: str,
+        openai_organization: Optional[str] = None,
         temperature: Optional[float] = None,
         top_p: Optional[float] = None,
         max_tokens: Optional[int] = None,
         presence_penalty: Optional[float] = None,
         frequency_penalty: Optional[float] = None,
+        num_completions: int = 1,
+        batch_size: int = 20,
+        max_retries: int = 6,
+        request_timeout: float = 600,
     ) -> None:
         """Initialize the LLM.
 
         Arguments:
             model: The model to use for the LLM.
-            open_ai_api_key: The OpenAI API key to use for the LLM.
-            organization: The OpenAI API organization.
-
-        Chat GPT Arguments:
+            openai_api_key: The OpenAI API key.
+            openai_organization: The OpenAI organization.
             temperature: The temperature to use for the LLM.
-            top_p: The top p to use for the LLM.
-            n: The number of outputs to generate per prompt.
-            stream: Whether to stream the output.
-            stop: A list of strings to stop the LLM from generating.
-            max_tokens: The maximum number of tokens to generate.
-            presence_penalty: The presence penalty to use for the LLM.
-            frequency_penalty: The frequency penalty to use for the LLM.
-            logit_bias: A dictionary of tokens to bias the output towards.
+            top_p: Total probability mass of tokens to consider at each step.
+            max_tokens: The maximum number of tokens to generate in the completion.
+                -1 returns as many tokens as possible given the prompt and
+                the models maximal context size.
+            presence_penalty: The presence penalty to use for the LLM. Penalizes repeated
+                tokens.
+            frequency_penalty: The frequency penalty to use for the LLM. Penalizes
+                repeated tokens according to frequency.
+            num_completions: The number of completions to generate for each prompt.
+            batch_size: Batch size to use when passing multiple documents to generate.
+            max_retries: The maximum number of retries to make when generating.
+            request_timeout: Timeout for requests to OpenAI completion API. Default is
+                600 seconds.
 
         See [here](https://platform.openai.com/docs/api-reference/chat/create) for more
         documentation on each variable.
@@ -80,20 +93,24 @@ class OpenAI:
                 f"Model {model} not supported. Select from one of the "
                 f"following: {list(MODEL_TYPES.keys())}"
             )
+        self._langchain_model = OpenAI(
+            model=model,
+            openai_api_key=openai_api_key,
+            openai_organization=openai_organization,
+            temperature=temperature,
+            max_tokens=-1,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty,
+            n=num_completions,
+            batch_size=batch_size,
+            max_retries=max_retries,
+        )
         self.model_max_tokens = TOKEN_LIMITS[model]
         self.model_type = MODEL_TYPES[model]
-        self.open_ai_api_key = open_ai_api_key
-        openai.api_key = open_ai_api_key
-        openai.organization = organization
+        self.openai_api_key = openai_api_key
 
-        # Set these to null if None so that they are not passed to the API
-        self.temperature = temperature
-        self.top_p = top_p
-        self.max_tokens = max_tokens
-        self.presence_penalty = presence_penalty
-        self.frequency_penalty = frequency_penalty
-
-    def get_output(self, prompt: str | List[Dict[str, str]]) -> str:
+    def get_output(self, prompt: str | Prompt) -> str:
         """Generate a single output from the LLM.
 
         Arguments:
@@ -102,133 +119,7 @@ class OpenAI:
         Returns:
             The output of the LLM.
         """
-        try:
-            if self.model_type == "chat-gpt":
-                return self._get_output_chat_gpt(prompt)
-            else:
-                return self._get_output_gpt_3(prompt)
-        except openai.error.RateLimitError:
-            log.warn(
-                "OpenAI API rate limit exceeded. Waiting 30 seconds and trying again."
-            )
-            time.sleep(30)
-            output = self.get_output(prompt)
-            return output
-        except openai.error.Timeout:
-            log.warn("OpenAI API timeout. Waiting 30 seconds and trying again.")
-            time.sleep(30)
-            output = self.get_output(prompt)
-            return output
-
-    def _get_output_chat_gpt(self, messages: List[Dict[str, str]]) -> str:
-        """Generate a single output from a 'chat-gpt' LLM.
-
-        Arguments:
-            messages: A list of messages to use for the LLM.
-
-        Returns:
-            The output of the 'chat-gpt' LLM.
-        """
-        args = [
-            self.temperature,
-            self.top_p,
-            self.max_tokens,
-            self.presence_penalty,
-            self.frequency_penalty,
-        ]
-        names = [
-            "temperature",
-            "top_p",
-            "max_tokens",
-            "presence_penalty",
-            "frequency_penalty",
-        ]
-
-        kwargs: Dict[str, int | float] = {}
-
-        for name, arg in zip(names, args):
-            if arg is not None:
-                kwargs[name] = arg
-
-        try:
-            response = openai.ChatCompletion.create(
-                model=self.model,
-                messages=messages,
-                **kwargs,
-            )
-        except openai.error.InvalidRequestError as e:
-            if e.code == "context_length_exceeded":
-                log.error(e.user_message)
-                log.error(f"Messages: {messages}")
-                raise e
-            else:
-                raise e
-
-        output = response["choices"][0]["message"]["content"]
-        tokens = response["usage"]
-        cost = COST_PER_MODEL[self.model]["input"] * (tokens["prompt_tokens"] / 1000)
-        cost += COST_PER_MODEL[self.model]["output"] * (
-            tokens["completion_tokens"] / 1000
-        )
-
-        return output, tokens, cost
-
-    def _get_output_gpt_3(self, prompt: str) -> str:
-        """Generate a single output from the LLM.
-
-        Arguments:
-            prompt: The prompt to use for the LLM.
-
-        Returns:
-            The output of the LLM.
-        """
-        output = openai.Completion.create(
-            model=self.model,
-            suffix=self.suffix,
-            prompt=prompt,
-            temperature=self.temperature,
-            max_tokens=self.max_tokens,
-            top_p=self.top_p,
-            frequency_penalty=self.frequency_penalty,
-            presence_penalty=self.presence_penalty,
-        )
-
-        return output["choices"][0]["text"]
-
-    def get_output_batch(
-        self, prompts: List[str] | List[List[Dict[str, str]]]
-    ) -> List[str]:
-        """Generate a batch of outputs from the LLM.
-
-        Chat GPT Arguments:
-            prompts: The prompts to use for the LLM.
-
-        Returns:
-            The outputs of the LLM.
-        """
-        log.info(f"Generating {len(prompts)} outputs with the following parameters: ")
-        log.info(f"Model: {self.model}")
-        log.info(f"Temperature: {self.temperature}")
-        log.info(f"Top P: {self.top_p}")
-        log.info(f"Max Tokens: {self.max_tokens}")
-        log.info(f"Presence Penalty: {self.presence_penalty}")
-        log.info(f"Frequency Penalty: {self.frequency_penalty}")
-
-        outputs = []
-
-        for prompt in prompts:
-            if self.model_type == "chat-gpt":
-                if not isinstance(prompt, list):
-                    raise ValueError(
-                        "Prompts must be a list of messages for chat-gpt models."
-                    )
-                outputs.append(self._get_output_chat_gpt(prompt))
-            else:
-                if not isinstance(prompt, str):
-                    raise ValueError("Prompts must be a string for non-chat-gpt models.")
-                outputs.append(self._get_output_gpt_3(prompt))
-
-        return outputs
+        output = self._langchain_model(prompt)
 
     def __str__(self) -> str:
         """Return a string representation of the LLM."""
