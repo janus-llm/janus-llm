0f3c0ac4f5732f214d2fc4d32aefb99f51a4b33b
Christopher Glasz <cglasz@mitre.org>
2023-09-15 15:40:05 -0400

    Update unit tests

--- janus/language/mumps/_tests/test_mumps.py
+++ janus/language/mumps/_tests/test_mumps.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from ....llm import MODEL_CONSTRUCTORS, MODEL_DEFAULT_ARGUMENTS
 from ..mumps import MumpsSplitter
+from ...combine import Combiner
 
 
 class TestMumpsSplitter(unittest.TestCase):
@@ -13,9 +14,12 @@ class TestMumpsSplitter(unittest.TestCase):
         model_name = "gpt-3.5-turbo"
         llm = MODEL_CONSTRUCTORS[model_name](**MODEL_DEFAULT_ARGUMENTS[model_name])
         self.splitter = MumpsSplitter(model=llm, max_tokens=2048)
+        self.combiner = Combiner(language="mumps")
         self.test_file = Path("janus/language/mumps/_tests/mumps.m")
 
     def test_split(self):
         """Test the split method."""
-        block = self.splitter.split(self.test_file)
-        self.assertEqual(len(block.children), 23)
+        split_code = self.splitter.split(self.test_file)
+        self.assertEqual(len(split_code.children), 22)
+        flat_split_text = self.combiner._blocks_to_str(split_code.code, split_code)
+        self.assertEqual(flat_split_text, self.test_file.read_text())
--- janus/language/treesitter/_tests/test_treesitter.py
+++ janus/language/treesitter/_tests/test_treesitter.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from ....llm import MODEL_CONSTRUCTORS, MODEL_DEFAULT_ARGUMENTS
 from ..treesitter import TreeSitterSplitter
+from ...combine import Combiner
 
 
 class TestTreeSitterSplitter(unittest.TestCase):
@@ -13,10 +14,10 @@ class TestTreeSitterSplitter(unittest.TestCase):
         model_name = "gpt-3.5-turbo"
         self.llm = MODEL_CONSTRUCTORS[model_name](**MODEL_DEFAULT_ARGUMENTS[model_name])
 
-    def _split(self) -> str:
+    def _split(self):
         """Split the test file."""
         split_code = self.splitter.split(self.test_file)
-        flat_split_text = self.splitter._blocks_to_str(split_code.code, split_code)
+        flat_split_text = self.combiner._blocks_to_str(split_code.code, split_code)
         # The newlines and spaces aren't the same but that doesn't matter for fortran
         # (to an extent)
         flat_split_text = flat_split_text.replace("\n", "").replace(" ", "")
@@ -26,11 +27,13 @@ class TestTreeSitterSplitter(unittest.TestCase):
     def test_split_fortran(self):
         """Test the split method."""
         self.splitter = TreeSitterSplitter(language="fortran", model=self.llm)
+        self.combiner = Combiner(language="fortran")
         self.test_file = Path("janus/language/treesitter/_tests/languages/fortran.f90")
         self._split()
 
     def test_split_matlab(self):
         """Test the split method."""
         self.splitter = TreeSitterSplitter(language="matlab", model=self.llm)
+        self.combiner = Combiner(language="matlab")
         self.test_file = Path("janus/language/treesitter/_tests/languages/matlab.m")
         self._split()
