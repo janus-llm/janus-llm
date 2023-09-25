a0bc24954a734b9740c3c2b9911871e14b6c2f58
Lily Wong <lwong@mitre.org>
2023-09-25 16:45:48 +0000

    Update llama and falcon model URLs, extend timeouts, add WizardCoder

--- janus/llm/models_info.py
+++ janus/llm/models_info.py
@@ -14,6 +14,7 @@ MODEL_CONSTRUCTORS: Dict[str, Any] = {
     "gpt-3.5-turbo-16k": ChatOpenAI,
     "llama": HuggingFaceTextGenInference,
     "falcon": HuggingFaceTextGenInference,
+    "wizard-coder": HuggingFaceTextGenInference,
 }
 
 _open_ai_defaults: Dict[str, Any] = {
@@ -27,22 +28,34 @@ MODEL_DEFAULT_ARGUMENTS: Dict[str, Dict[str, Any]] = {
     "gpt-3.5-turbo": dict(model_name="gpt-3.5-turbo", **_open_ai_defaults),
     "gpt-3.5-turbo-16k": dict(model_name="gpt-3.5-turbo-16k", **_open_ai_defaults),
     "llama": dict(
-        inference_server_url="https://llama-aip.lt.mitre.org",
+        inference_server_url="https://llama2-70b.aip.mitre.org",
         max_new_tokens=4096,
         top_k=10,
         top_p=0.95,
         typical_p=0.95,
         temperature=0.01,
         repetition_penalty=1.03,
+        timeout=240,
     ),
     "falcon": dict(
-        inference_server_url="https://falcon-aip.lt.mitre.org/",
+        inference_server_url="https://falcon-40b.aip.mitre.org",
         max_new_tokens=4096,
         top_k=10,
         top_p=0.95,
         typical_p=0.95,
         temperature=0.01,
         repetition_penalty=1.03,
+        timeout=240,
+    ),
+    "wizard-coder": dict(
+        inference_server_url="https://wizard-coder-34b.aip.mitre.org",
+        max_new_tokens=1024,
+        top_k=10,
+        top_p=0.95,
+        typical_p=0.95,
+        temperature=0.01,
+        repetition_penalty=1.03,
+        timeout=240,
     ),
 }
 
@@ -61,4 +74,5 @@ COST_PER_MODEL: Dict[str, Dict[str, float]] = {
     "gpt-3.5-turbo-16k": {"input": 0.003, "output": 0.004},
     "llama": {"input": 0.0, "output": 0.0},
     "falcon": {"input": 0.0, "output": 0.0},
+    "wizard-coder": {"input": 0.0, "output": 0.0},
 }
