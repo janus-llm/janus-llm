bc23d777b3e484f7af2a517eb5549f236e1388bb
William Macke <wmacke@mitre.org>
2024-03-14 16:15:54 -0400

    [WIP] added file pairing methods for pairing segments of files

--- janus/metrics/cli.py
+++ janus/metrics/cli.py
@@ -2,7 +2,9 @@ import click
 import typer
 from typing_extensions import Annotated
 
+from janus.llm import load_model
 from janus.metrics.file_pairing import FILE_PAIRING_METHODS
+from janus.utils.enums import LANGUAGES
 
 evaluate = typer.Typer(
     help="Evaluation",
@@ -18,9 +20,22 @@ def evaluate_main(
     src_file: Annotated[str, typer.Option()],
     cmp_file: Annotated[str, typer.Option()],
     out_file: Annotated[str, typer.Option()],
+    lang: Annotated[
+        str,
+        typer.Option(
+            help="The language of the source code.",
+            click_type=click.Choice(sorted(LANGUAGES)),
+        ),
+    ],
     file_pairing_method: str = typer.Option(
         default="PAIR_BY_FILE", click_type=click.Choice(FILE_PAIRING_METHODS.keys())
     ),
+    llm_name: Annotated[
+        str,
+        typer.Option(
+            help="The custom name of the model set with 'janus llm add'.",
+        ),
+    ] = "gpt-3.5-turbo",
 ):
     global state
     with open(src_file, "r") as f:
@@ -29,5 +44,9 @@ def evaluate_main(
     with open(cmp_file, "r") as f:
         cmp = f.read()
 
+    state["src_file"] = src_file
+    state["cmp_file"] = cmp_file
     state["out_file"] = out_file
-    state["pairs"] = FILE_PAIRING_METHODS[file_pairing_method](src, cmp)
+    state["lang"] = lang
+    state["llm"], state["token_limit"], state["model_cost"] = load_model(llm_name)
+    state["pairs"] = FILE_PAIRING_METHODS[file_pairing_method](src, cmp, state)
--- janus/metrics/file_pairing.py
+++ janus/metrics/file_pairing.py
@@ -1,4 +1,9 @@
-from typing import Callable, Dict, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+# from janus.language.binary import BinarySplitter
+# from janus.language.mumps import MumpsSplitter
+# from janus.language.treesitter import TreeSitterSplitter
+# from janus.utils.enums import CUSTOM_SPLITTERS
 
 FILE_PAIRING_METHODS: Dict[str, Callable[[str, str], List[Tuple[str, str]]]] = {}
 
@@ -16,10 +21,39 @@ def register_pairing_method(name: Optional[str] = None):
 
 
 @register_pairing_method()
-def PAIR_BY_FILE(src: str, cmp: str) -> List[Tuple[str, str]]:
+def PAIR_BY_FILE(src: str, cmp: str, state: Dict[str, Any]) -> List[Tuple[str, str]]:
     return [(src, cmp)]
 
 
 @register_pairing_method()
-def PAIR_BY_LINE(src: str, cmp: str) -> List[Tuple[str, str]]:
+def PAIR_BY_LINE(src: str, cmp: str, state: Dict[str, Any]) -> List[Tuple[str, str]]:
     return list(zip(src.split("\n"), cmp.split("\n")))
+
+
+# @register_pairing_method()
+# def PAIR_BY_LINE_COMMENT(
+#     src: str, cmp: str,
+#     state: Dict[str, Any]
+# ) -> List[Tuple[str, str]]:
+#     kwargs = dict(
+#         max_tokens=state["token_limit"] // 2.5,
+#         model=state["llm"],
+#         protected_node_types=tuple( ),
+#         prune_node_types=tuple(),
+#     )
+#     if state["lang"] in CUSTOM_SPLITTERS:
+#         if state["lang"] == "mumps":
+#             splitter = MumpsSplitter(**kwargs)
+#         elif state["lang"] == "binary":
+#             splitter = BinarySplitter(**kwargs)
+#     else:
+#         splitter = TreeSitterSplitter(language=state["lang"], **kwargs)
+#     src_tree = splitter.split(state["src_file"], prune_unprotected=False)
+#     cmp_tree = splitter.split(state["cmp_file"])
+#     def _print_tree(node, prefix):
+#         #print(prefix, node.node_type, node.complete_text)
+#         print(prefix, node.node_type)
+#         for c in node.children:
+#             _print_tree(c, prefix+"\t")
+#     _print_tree(src_tree, "")
+#     return [(src, cmp)]
