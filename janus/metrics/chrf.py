1e67761934b9778b811d517ba8141fdd6d22c2ff
Michael Doyle <mdoyle@mitre.org>
2024-03-22 12:49:55 -0700

    Use sacrebleu instead of torchmetrics

--- janus/metrics/chrf.py
+++ janus/metrics/chrf.py
@@ -1,22 +1,40 @@
 import typer
-from torchmetrics.text import CHRFScore
+from sacrebleu import sentence_chrf
 
 from .metric import metric
 
 
-@metric(help="chrF Score using Torchmetrics")
+@metric(help="chrF score using Torchmetrics")
 def chrf(
-    src: str,
-    dst: str,
-    n_char_order: int = typer.Option(default=6),
-    n_word_order: int = typer.Option(default=2),
-    beta: float = typer.Option(default=2.0),
-):
+    target: str,
+    reference: str,
+    n_char_order: int = typer.Option(
+        default=6,
+        help=(
+            "A character n-gram order. If n_char_order=6, the metrics refers to the "
+            "official chrF/chrF++."
+        ),
+    ),
+    n_word_order: int = typer.Option(
+        default=2,
+        help=(
+            "A word n-gram order. If n_word_order=2, the metric refers to the official "
+            "chrF++. If n_word_order=0, the metric is equivalent to the original ChrF."
+        ),
+    ),
+    beta: float = typer.Option(
+        default=2.0,
+        help=(
+            "Determines importance of recall w.r.t. precision. If beta=1, their "
+            "importance is equal."
+        ),
+    ),
+) -> float:
     """Calculate the chrF Score using Torchmetrics.
 
     Arguments:
-        src: The source text.
-        dst: The destination text.
+        target: The target text.
+        reference: The reference text.
         n_char_order: The character order.
         n_word_order: The word order.
         beta: The beta value.
@@ -24,9 +42,11 @@ def chrf(
     Returns:
         The chrF score.
     """
-    m = CHRFScore(
-        n_char_order=n_char_order,
-        n_word_order=n_word_order,
+    score = sentence_chrf(
+        target,
+        [reference],
+        char_order=n_char_order,
+        word_order=n_word_order,
         beta=beta,
     )
-    return float(m(src, dst).numpy())
+    return float(score.score)
