4633bfc4a554f99049110a7a04124eda5003b8b1
William Macke <wmacke@mitre.org>
2024-03-27 11:57:33 -0400

    fixed help messages

--- janus/metrics/bleu.py
+++ janus/metrics/bleu.py
@@ -9,23 +9,38 @@ from .metric import metric
 # from sacrebleu import sentence_chrf
 
 
-@metric()
+@metric(help="Compute the bleu score using sacrebleu")
 def bleu(
     target: str,
     reference: str,
     smooth_method: str = typer.Option(
-        default="exp", click_type=click.Choice(["exp", "floor", "add-k", "none"])
+        default="exp",
+        click_type=click.Choice(["exp", "floor", "add-k", "none"]),
+        help="smoothing method to use.",
     ),
     smooth_value: Optional[float] = typer.Option(
         default=None,
+        help="smoothing value (only for floor and add-k).",
     ),
     lowercase: bool = typer.Option(
         default=False,
+        help="whether to lowercase the data.",
     ),
     use_effective_order: bool = typer.Option(
         default=True,
+        help="Don't use n-gram orders without matches.",
     ),
 ):
+    """
+    Computes bleu score using sacrebleu
+    Arguments:
+        target: The target text.
+        reference: The reference text.
+        smooth_method: smoothing method to use.
+        smooth_value: smoothing value (only for floor and add-k).
+        lowercase: whether to lowercase the data.
+        use_effective_order: Don't use n-gram orders without matches.
+    """
     score = sentence_bleu(
         target,
         [reference],
