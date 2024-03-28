7e1e94ae6d234bd590da0adf0029a5c1ded3e077
Paul Kirk <pkirk@mitre.org>
2024-03-27 21:43:11 -0400

    Resolve "Add readability metric to `janus evaluate`"

---
+++ janus/metrics/reading.py
@@ -0,0 +1,45 @@
+import nltk
+import readability
+
+from .metric import metric
+
+
+def get_readability(target: str) -> readability.Readability:
+    """Create a Readability object from an input string
+
+    Arguments:
+        target: The target text.
+
+    Returns:
+        py-readability-metrics Readability object for that text
+    """
+    nltk.download("punkt", quiet=True)
+    return readability.Readability(target)
+
+
+@metric(use_reference=False, help="The Flesch Readability score of text")
+def flesch(target: str) -> float:
+    """Calculate the Flesch Score using py-readability-metrics.
+
+    Arguments:
+        target: The target text.
+
+    Returns:
+        The Flesch score.
+    """
+
+    return get_readability(target).flesch().score
+
+
+@metric(use_reference=False, help="The Gunning-Fog Readability score of text")
+def gunning_fog(target: str) -> float:
+    """Calculate the Gunning-Fog Score using py-readability-metrics.
+
+    Arguments:
+        target: The target text.
+
+    Returns:
+        The Gunning-Fog score.
+    """
+
+    return get_readability(target).gunning_fog().score
