0893a226fbc918ec48ae25c5d8a861066b2a5d19
Michael Doyle <mdoyle@mitre.org>
2024-04-09 15:06:16 -0700

    Update cli.py and tests for it

--- janus/_tests/test_cli.py
+++ janus/_tests/test_cli.py
@@ -3,6 +3,7 @@ import unittest
 from typer.testing import CliRunner
 
 from ..cli import app
+from ..llm.models_info import MODEL_CONFIG_DIR
 
 
 class TestCli(unittest.TestCase):
@@ -23,4 +24,44 @@ class TestCli(unittest.TestCase):
 
     def test_no_arguments(self):
         result = self.runner.invoke(app)
+        self.assertEqual(result.exit_code, 0)
+
+    def test_translate_help(self):
+        result = self.runner.invoke(app, ["translate", "-h"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_document_help(self):
+        result = self.runner.invoke(app, ["document", "-h"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_db_help(self):
+        result = self.runner.invoke(app, ["db", "-h"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_llm_help(self):
+        result = self.runner.invoke(app, ["llm", "-h"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_llm_add(self):
+        llm_model_path = MODEL_CONFIG_DIR / "test-model-name.json"
+        if llm_model_path.exists():
+            llm_model_path.unlink()
+        result = self.runner.invoke(app, ["llm", "add", "test-model-name"])
+        self.assertEqual(result.exit_code, 0)
+        self.assertTrue(llm_model_path.exists())
+        llm_model_path.unlink()
+
+    def test_db_ls(self):
+        result = self.runner.invoke(app, ["db", "ls"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_db_status(self):
+        result = self.runner.invoke(app, ["db", "status"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_db_add_and_rm(self):
+        result = self.runner.invoke(app, ["db", "add", "test-db-name"])
+        self.assertEqual(result.exit_code, 0)
+
+        result = self.runner.invoke(app, ["db", "rm", "test-db-name", "-y"])
         self.assertEqual(result.exit_code, 0)
