c3b256f0bef3fb25c21653dfa7eccf66c782885b
William Macke <wmacke@mitre.org>
2024-04-09 08:41:00 -0400

    [WIP] added missing file

---
+++ janus/metrics/similarity.py
@@ -0,0 +1,12 @@
+from langchain.evaluation import load_evaluator
+
+from janus.embedding.embedding_models_info import load_embedding_model
+
+from .metric import metric
+
+
+@metric()
+def similarity_score(targ: str, ref: str, **kwargs):
+    embedding_model, _, _ = load_embedding_model("gpt-3.5-turbo")
+    evaluator = load_evaluator("pairwise_embedding_distance", embeddings=embedding_model)
+    return evaluator.evaluate_string_pairs(prediction=targ, prediction_b=ref)["score"]
