3f61c3e5788f6fbf2b8f53d9665ac8087a3676b5
William Macke <wmacke@mitre.org>
2024-04-09 13:37:53 -0400

    similarity score is working

--- janus/metrics/similarity.py
+++ janus/metrics/similarity.py
@@ -7,6 +7,6 @@ from .metric import metric
 
 @metric()
 def similarity_score(targ: str, ref: str, **kwargs):
-    embedding_model, _, _ = load_embedding_model("gpt-3.5-turbo")
+    embedding_model, _, _ = load_embedding_model("text-embedding-3-small")
     evaluator = load_evaluator("pairwise_embedding_distance", embeddings=embedding_model)
     return evaluator.evaluate_string_pairs(prediction=targ, prediction_b=ref)["score"]
