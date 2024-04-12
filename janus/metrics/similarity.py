c6005d176a24a12f47b3123f50c0bfd000e90a9a
Michael Doyle <mdoyle@mitre.org>
2024-04-12 10:17:08 -0700

    Update similarity score docstring and variable names

--- janus/metrics/similarity.py
+++ janus/metrics/similarity.py
@@ -8,33 +8,40 @@ from janus.embedding.embedding_models_info import load_embedding_model
 from .metric import metric
 
 
-@metric(name="similarity-score", help="Distance between embeddings of strings")
+@metric(name="similarity-score", help="Distance between embeddings of strings.")
 def similarity_score(
-    targ: str,
-    ref: str,
+    target: str,
+    reference: str,
     model_name: Annotated[
         str,
-        typer.Option("-em", "--embedding-model", help="Name of embedding model to use"),
+        typer.Option("-m", "--model", help="Name of embedding model to use."),
     ] = "text-embedding-3-small",
     distance_metric: Annotated[
         str,
         typer.Option(
-            "-dm",
+            "-d",
             "--distance-metric",
             click_type=click.Choice([e.value for e in list(EmbeddingDistance)]),
-            help="Distance metric to use",
+            help="Distance metric to use.",
         ),
     ] = "cosine",
     **kwargs,
 ) -> float:
-    """
-    Computes the similarity score of two strings
-    :param targ target string to evaluate
-    :param ref reference string
-    :param model_name name of embedding model to use
-    :param distance metric name of distance metric to use
-    :param kwargs key word arguments
+    """Computes the similarity score of two strings
 
+    Arguments:
+        target: The target string.
+        reference: The reference string.
+        model_name: The name of the embedding model to use.
+        distance_metric: The distance metric to use. Can be one of:
+            - cosine
+            - euclidean
+            - manhattan
+            - chebyshev
+            - hamming
+
+    Returns:
+        The similarity score of the two strings.
     """
     embedding_model, _, _ = load_embedding_model(model_name)
     evaluator = load_evaluator(
@@ -42,4 +49,6 @@ def similarity_score(
         embeddings=embedding_model,
         distance_metric=distance_metric,
     )
-    return evaluator.evaluate_string_pairs(prediction=targ, prediction_b=ref)["score"]
+    return evaluator.evaluate_string_pairs(prediction=target, prediction_b=reference)[
+        "score"
+    ]
