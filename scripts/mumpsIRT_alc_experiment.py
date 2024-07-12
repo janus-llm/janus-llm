ba7182a594a9dc0524627f9a103f9d12f65a9338
Parthav Poudel <ppoudel@mitre.org>
2024-07-12 13:17:06 -0400

    Documentation & cleanup

--- scripts/mumpsIRT_alc_experiment.py
+++ scripts/mumpsIRT_alc_experiment.py
@@ -11,26 +11,45 @@ class Experimenter:
 
     """
     A class that conducts a series of comment generation experiments using different
-    partitioning methods and token sizes for mumps code
+    partitioning methods and token sizes for mumps or alc code.
     """
     def __init__(self,
                 input_dir: str | None=None,
                 source_language: str | None=None,
-                model: str = "bedrock-llama3-70b-instruct",
+                model: str = "gpt-3.5-turbo-0125",
                 TOK_SIZES : list = [512, 1024, 2048, 4096, 8192, 16384, 100000, 200000],
                 RESULT_DIRS : list = [
                                 "ast-strict-results",
                                 "file-results",
-                                "tag-results",
                                 "ast-flex-results-",
                                 "chunk-results-",
                             ]
                 ):
+        
+        """
+        Initialize an Experimenter instance. 
+
+        Arguments:
+            input_dir: Directory containing the input source code files.
+            source_langauge: The language of the source code files.
+                NOTE: If doing alc experiment, source langauge should be ibmhlasm
+            model: The LLM to use for comment generation.
+            TOK_SIZES: The varying tokens limits which certain splitting methods require.
+                NOTE: Only the AST-FLEX and CHUNK splitting methods use varying token limits
+                and that is hard coded into this script
+            RESULT_DIRS: The names of the resulting output directories
+        """
         self.input_dir = input_dir
         self.source_language = source_language
         self.model = model
         self.TOK_SIZES = TOK_SIZES
-        self.RESULT_DIRS = RESULT_DIRS
+        if source_language == "mumps":
+            self.RESULT_DIRS = RESULT_DIRS 
+        else:
+            self.RESULT_DIRS = [ 
+                "file-results",
+                "chunk-results-"
+            ]
 
     def run(self):
         kwargs = dict(
@@ -40,97 +59,86 @@ class Experimenter:
             max_tokens=1000000,
             comments_per_request=100,
         )
-
         # Documenters with fixed 1,000,000 token limit
-        ast_strict_split = MadLibsDocumenter(custom_splitter="ast-strict", **kwargs)
-
-        log.info("Running AST-STRICT splitting experiment.")
-        ast_strict_split.translate(
+        file_split = MadLibsDocumenter(custom_splitter="file", **kwargs)
+        log.info("Running NO (FILE) splitting experiment.")
+        file_split.translate(
             input_directory=self.input_dir,
-            output_directory="ast-strict-results",
+            output_directory=self.source_language + "-file-results",
         )
-        log.info("AST-STRICT splitting experiment complete.")
+        log.info("NO (FILE) splitting experiment complete")
 
-        # Only run FILE and TAG split if source language is mumps
         if self.source_language == "mumps":
-            file_split = MadLibsDocumenter(custom_splitter="file", **kwargs)
-            log.info("Running NO (FILE) splitting experiment.")
-            file_split.translate(
+            ast_strict_split = MadLibsDocumenter(custom_splitter="ast-strict", **kwargs)
+            log.info("Running AST-STRICT splitting experiment.")
+            ast_strict_split.translate(
                 input_directory=self.input_dir,
-                output_directory="file-results",
+                output_directory=self.source_language + "-ast-strict-results",
             )
-            log.info("NO (FILE) splitting experiment complete")
+            log.info("AST-STRICT splitting experiment complete.")
 
-            tag_split = MadLibsDocumenter(custom_splitter="tag", **kwargs)
-            log.info("Running TAG splitting experiment.")
-            tag_split.translate(
-                input_directory=self.input_dir,
-                output_directory="tag-results",
-            )
-            log.info("TAG splitting experiment complete.")
 
         # For running documenters that require varying token limits
         for TOKS in self.TOK_SIZES:
             kwargs["max_tokens"] = TOKS
-
-            ast_flex_split = MadLibsDocumenter(custom_splitter="ast-flex", **kwargs)
-            log.info(f"Running AST-FLEX splitting experiment with {TOKS} as max token limit for model.")
-            ast_flex_split.translate(
+        
+            chunk_split = MadLibsDocumenter(custom_splitter="chunk", **kwargs)
+            log.info(f"Running CHUNK splitting experiment with {TOKS} as max token limit for model.")
+            chunk_split.translate(
                 input_directory=self.input_dir,
-                output_directory="ast-flex-results-" + str(TOKS),
+                output_directory=self.source_language + "-chunk-results-" + str(TOKS),
             )
-            log.info(f"AST-FLEX splitting experiment with {TOKS} as max token limit complete.")
+            log.info(f"CHUNK splitting experiment with {TOKS} as max token limit complete.")
 
-            # Only run CHUNK split if source language is MUMPS
             if self.source_language == "mumps":
-                chunk_split = MadLibsDocumenter(custom_splitter="chunk", **kwargs)
-                log.info(f"Running CHUNK splitting experiment with {TOKS} as max token limit for model.")
-                chunk_split.translate(
+                ast_flex_split = MadLibsDocumenter(custom_splitter="ast-flex", **kwargs)
+                log.info(f"Running AST-FLEX splitting experiment with {TOKS} as max token limit for model.")
+                ast_flex_split.translate(
                     input_directory=self.input_dir,
-                    output_directory="chunk-results-" + str(TOKS),
+                    output_directory=self.source_language + "-ast-flex-results-" + str(TOKS),
                 )
-                log.info(f"CHUNK splitting experiment with {TOKS} as max token limit complete.")
+                log.info(f"AST-FLEX splitting experiment with {TOKS} as max token limit complete.")
 
     def process_dirs(self):
-        input_dir = Path(self.input_dir).expanduser()
+        input_file = Path(self.input_dir + "/processed.json").expanduser()
         for DIR in self.RESULT_DIRS:
-            if DIR.startswith("ast-flex", "chunk"):
+            if "ast-flex" in DIR or "chunk" in DIR:
                 for TOK in self.TOK_SIZES:
-                    log.info(f"Combining comment jsons for {DIR}")
-                    output_dir = Path(DIR + str(TOK)).expanduser()
-                    obj = parse_madlibs(input_dir, output_dir)
+                    log.info(f"Combining comment jsons for {DIR}-{TOK}")
+                    output_dir = Path(f"{self.source_language}-{DIR}" + str(TOK)).expanduser()
+                    obj = parse_madlibs(input_file, output_dir)
                     (output_dir / "processed.json").write_text(json.dumps(obj, indent=2))
             else:
                 log.info(f"Combining comment jsons for {DIR}")
-                output_dir = Path(DIR).expanduser()
-                obj = parse_madlibs(input_dir, output_dir)
+                output_dir = Path(f"{self.source_language}-{DIR}").expanduser()
+                obj = parse_madlibs(input_file, output_dir)
                 (output_dir / "processed.json").write_text(json.dumps(obj, indent=2))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="Mumps comment generation experiment",
-        description=" conducts a series of comment generation experiments using different"
-        "partitioning methods and token sizes for mumps code.",
+        prog="Mumps & ALC comment generation experiment",
+        description="Conducts a series of comment generation experiments using different"
+        "partitioning methods and token sizes for mumps and alc code.",
     )
 
     parser.add_argument(
         "--input-dir",
         type=str,
-        # required=True, #! TESTING PURPOSE..RM CMT
-        default="/Users/ppoudel/Documents/modern_eval/llm-data/ITMod/documentation-tests/madlibs/alc-walmart/comments-removed" #! TESTING PURPOSE..RM
+        required=True,
+        help="Input directory of source code files.",
     )
 
     parser.add_argument(
         "--source-language",
-        tyoe=str,
-        # required=True, #! TESTING PURPOSE RM CMT
-        default='alc' #! TESTING PURPOSE RM 
+        type=str,
+        required=True,
+        help="If doing alc experiment, source langauge should be ibmhlasm, otherwise use mumps.",
     )
 
     parser.add_argument(
         "--model",
         type=str,
-        default="bedrock-llama3-70b-instruct",
+        default="gpt-3.5-turbo-0125",
         help="The LLM model to be used for comment generation",
     )
 
@@ -139,6 +147,7 @@ if __name__ == "__main__":
         nargs="+",
         type=int,
         default=[512, 1024, 2048, 4096, 8192, 16384, 100000, 200000],
+        help="Specify the varyingmax tokens sizes for the ast-flex and chunk splitting method"
     )
 
     args = parser.parse_args()
