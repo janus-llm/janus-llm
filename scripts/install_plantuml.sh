70ff97847f0190675488fb41865aa7c58d4c1307
William Macke <wmacke@mitre.org>
2024-07-30 16:56:26 -0400

    fixed install script

--- scripts/install_plantuml.sh
+++ scripts/install_plantuml.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 mkdir -p $HOME/.janus
-mkdir -p $HOME/.janus/bin
 mkdir -p $HOME/.janus/lib
-script_dir=$(dirname $0)
 wget -O $HOME/.janus/lib/plantuml.jar https://github.com/plantuml/plantuml/releases/download/v1.2024.6/plantuml-mit-1.2024.6.jar
-#cp $script_dir/plantuml $HOME/.janus/bin
-#chmod +x $HOME/.janus/bin/plantuml
-#echo "add PATH=\$PATH:$HOME/.janus/bin to your .bashrc or .zshrc file"
