58d7aab0faf5d67fe7b8826c73a97c5b7b1e6ca6
William Macke <wmacke@mitre.org>
2024-08-01 16:07:47 -0400

    [WIP] minor update to refiner class

--- janus/refiners/refiner.py
+++ janus/refiners/refiner.py
@@ -1,10 +1,14 @@
 class Refiner:
-    def __init__(
-        self,
-        model,
-        parser,
-    ):
-        pass
-
     def refine(self, original_prompt, original_output, errors, **kwargs):
+        """
+        Creates a new prompt based on feedback from original results
+        Arguments:
+            original_prompt: original prompt used to produce output
+            original_output: origial output of llm
+            errors: list of errors detected by parser
+        """
         raise NotImplementedError
+
+
+class BasicRefiner:
+    pass
