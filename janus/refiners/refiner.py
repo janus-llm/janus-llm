794a943f12ef0b8a6023753ce915407ff7d0f2b1
William Macke <wmacke@mitre.org>
2024-08-02 14:12:33 -0400

    added basic refiner class definition

--- janus/refiners/refiner.py
+++ janus/refiners/refiner.py
@@ -1,3 +1,6 @@
+from janus.llm.models_info import MODEL_PROMPT_ENGINES
+
+
 class Refiner:
     def refine(self, original_prompt, original_output, errors, **kwargs):
         """
@@ -11,4 +14,19 @@ class Refiner:
 
 
 class BasicRefiner:
-    pass
+    def __init__(
+        self,
+        prompt_name,
+        model_name,
+    ):
+        self._prompt_name = prompt_name
+        self._model_name = model_name
+
+    def refine(self, original_prompt, original_output, errors, **kwargs):
+        prompt_engine = MODEL_PROMPT_ENGINES[self._model_name](
+            prompt_template=self._prompt_name,
+            original_prompt=original_prompt,
+            original_output=original_output,
+            errors=errors,
+        )
+        return prompt_engine.prompt
