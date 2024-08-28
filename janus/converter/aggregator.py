5f9af8907a842dc80eb43b91ddf79f4fbe347173
William Macke <wmacke@mitre.org>
2024-08-28 17:26:22 -0400

    aggregator passes first test

--- janus/converter/aggregator.py
+++ janus/converter/aggregator.py
@@ -18,8 +18,8 @@ class Aggregator(Converter):
         self._load_parameters()
 
     def _iterative_translate(self, root: CodeBlock) -> TranslatedCodeBlock:
-        res = TranslatedCodeBlock(root)
-        self._recursive_translate(res)
+        res = TranslatedCodeBlock(root, language=self._target_language)
+        return self._recursive_translate(res)
 
     def _recursive_translate(self, root: TranslatedCodeBlock) -> None:
         if len(root.children) > 0:
@@ -27,12 +27,12 @@ class Aggregator(Converter):
                 self._recursive_translate(c)
             root.original.text = self._combine_blocks(root.children, self._separator)
         else:
-            int_reps = [
-                ic._add_translation(deepcopy(root))
-                for ic in self._intermediate_converters
-            ]
+            int_reps = [deepcopy(root) for ic in self._intermediate_converters]
+            for ic, r in zip(self._intermediate_converters, int_reps):
+                ic._add_translation(r)
             root.original.text = self._combine_blocks(int_reps, self._separator)
         self._add_translation(root)
+        return root
 
     def _combine_blocks(self, blocks: List[TranslatedCodeBlock], separator: str) -> str:
         return separator.join([block.text for block in blocks])
