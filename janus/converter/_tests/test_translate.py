832f87e64e75a0fde8c20ad25e361524db3e6fc9
William Macke <wmacke@mitre.org>
2025-01-23 16:50:47 -0500

    updated test

--- janus/converter/_tests/test_translate.py
+++ janus/converter/_tests/test_translate.py
@@ -66,7 +66,7 @@ class TestTranslator(unittest.TestCase):
     def test_translate(self):
         """Test translate method."""
         # Delete a file if it's already there
-        python_file = self.test_file.parent / "python" / f"{self.test_file.stem}.py"
+        python_file = self.test_file.parent / "python" / f"{self.test_file.stem}.json"
         python_file.unlink(missing_ok=True)
         python_file.parent.rmdir() if python_file.parent.is_dir() else None
         self.translator.translate(self.test_file.parent, self.test_file.parent / "python")
