3ba1e1417c4371b0ac73ba166a2266bdb8dcd70f
William Macke <wmacke@mitre.org>
2025-02-12 09:45:38 -0500

    added converter passthrough for passing codeblocks forward

--- janus/converter/__init__.py
+++ janus/converter/__init__.py
@@ -3,6 +3,7 @@ from janus.converter.diagram import DiagramGenerator
 from janus.converter.document import ClozeDocumenter, Documenter, MultiDocumenter
 from janus.converter.evaluate import Evaluator
 from janus.converter.partition import Partitioner
+from janus.converter.passthrough import ConverterPassthrough
 from janus.converter.pool import ConverterPool
 from janus.converter.requirements import RequirementsDocumenter
 from janus.converter.translate import Translator
