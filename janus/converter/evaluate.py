969f6a293b8908e9a873f101408262697a7bbc5e
Michael Doyle <mdoyle@mitre.org>
2025-02-28 17:53:02 -0800

    Fix error with nested requirements list and llm self eval parsing

--- janus/converter/evaluate.py
+++ janus/converter/evaluate.py
@@ -94,6 +94,9 @@ class RequirementEvaluator(Evaluator):
         else:
             input_str = input_block.previous_generations[-1].original.text
         requirements = json.loads(input_block.text)
+        # The requirements are often a list of lists
+        if isinstance(requirements[0], list):
+            requirements = requirements[0]
         if not requirements:
             log.debug(f"[{input_block.name}] Skipping empty output")
             return []
