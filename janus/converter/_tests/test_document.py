b9cfe98ed5ed8caaab42ed4596da908ba1bd4ea0
Son Huynh <shuynh@mitre.org>
2025-03-25 10:42:59 -0400

    Add unit test for pseudocode documenter

---
+++ janus/converter/_tests/test_document.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import tempfile
+import unittest
+
+from janus.converter.document import PseudocodeDocumenter
+
+
+class TestDocumenter(unittest.TestCase):
+    """Tests for the Documenter class"""
+
+    def test_pseudocode(self):
+        """Test pseudocode documenter"""
+        test_file = Path("janus/language/treesitter/_tests/languages/ibmhlasm.asm")
+
+        with tempfile.TemporaryDirectory(dir=test_file.parent) as tmpdirname:
+            python_file = Path(tmpdirname) / f"{test_file.stem}.json"
+
+            documenter = PseudocodeDocumenter(
+                model="gpt-4o-mini", source_language="ibmhlasm"
+            )
+            documenter.translate(test_file.parent, tmpdirname)
+            # Only check the top-most level functionality, since it should be handled by other
+            # unit tests anyway
+            self.assertTrue(python_file.exists())
