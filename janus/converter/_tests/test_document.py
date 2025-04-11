8962f0916e7b15e7db6e3ef58b629872de624893
Son Huynh <shuynh@mitre.org>
2025-04-11 09:18:59 -0400

    Write unit test for cloze documenter

--- janus/converter/_tests/test_document.py
+++ janus/converter/_tests/test_document.py
@@ -4,11 +4,53 @@ import unittest
 from pathlib import Path
 from unittest.mock import patch
 
-from janus.converter.document import PseudocodeDocumenter
+from janus.converter.document import ClozeDocumenter, PseudocodeDocumenter
+from janus.language.block import CodeBlock, TranslatedCodeBlock
 
 
 class TestDocumenter(unittest.TestCase):
     """Tests for the Documenter class"""
+
+    @patch("janus.converter.Converter._add_translation")
+    @patch("janus.parsers.doc_parser.ClozeDocumentationParser.parse")
+    def test_cloze(self, mock_parse, mock_super_add_translation):
+        """Test cloze documenter"""
+        documenter = ClozeDocumenter(model="gpt-4o-mini", source_language="ibmhlasm")
+        documenter.comments_per_request = 1
+
+        code_block = CodeBlock(
+            id="test",
+            name="Test Block",
+            node_type="function",
+            language="ibmhlasm",
+            text="*\n* <BLOCK_COMMENT 14b80530>\nDFHEISTG DSECT<INLINE_COMMENT dadfa102>",
+            start_point=(0, 0),
+            end_point=(1, 0),
+            start_byte=0,
+            end_byte=1,
+            tokens=0,
+            children=[],
+            previous_generations=[],
+        )
+
+        mock_super_add_translation.return_value = None
+
+        mock_parse.side_effect = [
+            '{"14b80530": "first line"}',
+            '{"dadfa102": "second line"}',
+            "{}",
+        ]
+
+        documenter._add_translation(
+            TranslatedCodeBlock(code_block, documenter.target_language, documenter)
+        )
+
+        self.assertEqual(3, mock_parse.call_count)
+
+        expectedJsonObj = '{"14b80530": "first line", "dadfa102": "second line"}'
+
+        # verify that comments are joined back together properly
+        mock_parse.assert_called_with(expectedJsonObj)
 
     @patch("janus.converter.Converter._run_chain")
     @patch("time.time")
