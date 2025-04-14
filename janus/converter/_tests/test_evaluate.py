0089f3215d8482cc0b4153558531fc4115023d95
Son Huynh <shuynh@mitre.org>
2025-04-14 11:31:58 -0400

    Write test for Summary + UML evaluators

--- janus/converter/_tests/test_evaluate.py
+++ janus/converter/_tests/test_evaluate.py
@@ -1,7 +1,11 @@
 import unittest
 from unittest.mock import call, patch
 
-from janus.converter.evaluate import RequirementEvaluator, UMLEvaluator
+from janus.converter.evaluate import (
+    RequirementEvaluator,
+    SummaryEvaluator,
+    UMLEvaluator,
+)
 from janus.language.block import CodeBlock, TranslatedCodeBlock
 from janus.refiners.refiner import FixParserExceptions
 
@@ -11,7 +15,7 @@ class TestUMLEvaluator(unittest.TestCase):
 
     def setUp(self):
         """Set up the tests"""
-        self.uml_evaluator = UMLEvaluator(
+        self.evaluator = UMLEvaluator(
             model="gpt-4o",
             source_language="json",
             refiner_types=[FixParserExceptions],
@@ -20,21 +24,23 @@ class TestUMLEvaluator(unittest.TestCase):
 
     def test_init(self):
         """Test __init__ method."""
-        self.assertEqual(self.uml_evaluator._model_name, "gpt-4o")
-        self.assertEqual(self.uml_evaluator._source_language, "json")
-        self.assertEqual(self.uml_evaluator._use_janus_inputs, True)
+        self.assertEqual(self.evaluator._model_name, "gpt-4o")
+        self.assertEqual(self.evaluator._source_language, "json")
+        self.assertEqual(self.evaluator._use_janus_inputs, True)
 
-    def test_partial_translate_block(self):
-        """Test part of the translate_block method in the
-        UMLEvaluator. We aren't testing the full method, since that requires
-        sending an LLM request."""
+    @patch("janus.converter.Converter._split_text")
+    @patch("janus.converter.Converter.translate_block")
+    def test_translate_block(self, mock_translate_block, mock_split_text):
+        """Test translate_block method"""
+
+        self.evaluator._use_janus_inputs = False
 
         source = CodeBlock(
             id="test",
             name="Test Block",
             node_type="function",
             language="json",
-            text="",
+            text="This is UML",
             start_point=(0, 0),
             end_point=(1, 0),
             start_byte=0,
@@ -44,11 +50,19 @@ class TestUMLEvaluator(unittest.TestCase):
             previous_generations=[{"input": "test"}],
         )
 
-        self.assertEqual(self.uml_evaluator.translate_block(source), [])
+        translated_block = TranslatedCodeBlock(source, source.language, self.evaluator)
+        mock_translate_block.return_value = translated_block
+
+        actual = self.evaluator.translate_block(source)
+        obj_str = '{"diagrams": "This is UML", "code": "test"}'
+        mock_split_text.assert_called_with(obj_str, source.name)
+
+        self.assertEqual(actual.original, source)
+        self.assertEqual(actual.previous_generations, source.previous_generations)
 
 
 class TestRequirementEvaluator(unittest.TestCase):
-    """Tests for the UMLEvaluator class"""
+    """Tests for the RequirementEvaluator class"""
 
     def setUp(self):
         """Set up the tests"""
@@ -151,3 +165,54 @@ class TestRequirementEvaluator(unittest.TestCase):
         )
 
         self.assertEqual(actual.text, '{"a": "b", "b": "c"}')
+
+
+class TestSummaryEvaluator(unittest.TestCase):
+    """Tests for the SummaryEvaluator class"""
+
+    def setUp(self):
+        """Set up the tests"""
+        self.evaluator = SummaryEvaluator(
+            model="gpt-4o",
+            source_language="json",
+            refiner_types=[FixParserExceptions],
+            use_janus_inputs=True,
+        )
+
+    def test_init(self):
+        """Test __init__ method."""
+        self.assertEqual(self.evaluator._model_name, "gpt-4o")
+        self.assertEqual(self.evaluator._source_language, "json")
+        self.assertEqual(self.evaluator._use_janus_inputs, True)
+
+    @patch("janus.converter.Converter._split_text")
+    @patch("janus.converter.Converter.translate_block")
+    def test_translate_block(self, mock_translate_block, mock_split_text):
+        """Test translate_block method"""
+
+        self.evaluator._use_janus_inputs = False
+
+        source = CodeBlock(
+            id="test",
+            name="Test Block",
+            node_type="function",
+            language="json",
+            text="This is a summary",
+            start_point=(0, 0),
+            end_point=(1, 0),
+            start_byte=0,
+            end_byte=1,
+            tokens=5,
+            children=[],
+            previous_generations=[{"input": "test"}],
+        )
+
+        translated_block = TranslatedCodeBlock(source, source.language, self.evaluator)
+        mock_translate_block.return_value = translated_block
+
+        actual = self.evaluator.translate_block(source)
+        obj_str = '{"summary": "This is a summary", "code": "test"}'
+        mock_split_text.assert_called_with(obj_str, source.name)
+
+        self.assertEqual(actual.original, source)
+        self.assertEqual(actual.previous_generations, source.previous_generations)
