0f4087db3479c39670e7f9f143c2442757c1a12a
Christopher Glasz <cglasz@mitre.org>
2025-05-07 10:05:31 -0400

    Simplify UML parsing logic

--- janus/parsers/eval_parsers/uml_parser.py
+++ janus/parsers/eval_parsers/uml_parser.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.exceptions import OutputParserException
@@ -34,31 +35,11 @@ class UMLParser(JanusParser, PydanticOutputParser):
         text = super().parse_input(block)
         inputs = json.loads(text)
         # strong assumption every @startuml has an @enduml, aka valid uml
-        diagram_str = inputs["eval_object"]
-        start_indicies = []
-        end_indicies = []
-        start = 0
-        index = 0
-        end = len(diagram_str)
-        while index != -1:
-            index = diagram_str.find("@st", start)
-            if index != -1:
-                start_indicies.append(index)
-                start = index + 3
-                temp_idx = diagram_str[:end].rfind("@enduml")
-                end_indicies.append(temp_idx + 6)
-                end = end_indicies[-1] - 3 - 6
-
-        if len(start_indicies) == 1:
-            inputs["eval_object"] = [diagram_str]
-            return json.dumps(inputs)
-
-        diagram_list = []
-        for i, idx in enumerate(start_indicies):
-            end_idx = end_indicies[-1 - i]
-            end_idx += 1
-            diagram_list.append(diagram_str[idx:end_idx])
-        inputs["eval_object"] = diagram_list
+        inputs["eval_object"] = re.findall(
+            r"@startuml.*?@enduml",
+            inputs["eval_object"],
+            flags=re.DOTALL,
+        )
         return json.dumps(inputs)
 
     def parse(self, text: str | BaseMessage) -> str:
